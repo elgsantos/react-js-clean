13b7b90a080295246895a5a7fff8150eb9d04d03
Eduardo Santos <elgsantos@gmail.com>
2020-12-31 15:17:25 -0300

    feat: ensure ValidationComposite returns error ir any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,15 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validators: FieldValidation[]) {}
+  validate (fieldName: string, fieldValue: string): string {
+    const fieldValidators = this.validators.filter(v => v.field === fieldName)
+    for (const validator of fieldValidators) {
+      const error = validator.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
+  }
+}
