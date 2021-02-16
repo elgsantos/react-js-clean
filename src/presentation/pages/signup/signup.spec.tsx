40aa10c6edb8990dfd6128af8b6f84fedd074c71
Eduardo Santos <elgsantos@gmail.com>
2021-02-16 12:03:47 -0300

    test: ensure SignUp show valid name status if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -66,4 +66,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
+
+  test('Should show valid name state if Validation succeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name')
+  })
 })
